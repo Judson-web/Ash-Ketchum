eaaea85faea73e7eece10a097d689bb2c3f2fd31
KING XV <85796566+Judson-web@users.noreply.github.com>
2021-09-19 17:19:04 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,20 +1,20 @@
 class Translation(object):
-    START_TEXT = """ <b> Hi {} ,  I am a telegram renamer
+    START_TEXT = """ <b> Hi {} ,  I am a 𝘼𝙨𝙝 𝙆𝙚𝙩𝙘𝙝𝙪𝙢
 You can rename any type of file or media with permanent thumbnail support.
 
 Go to /help and choose appropriate modes</b> \n
 
-<b>Bot Maintained By: @VAMPIRE_KING_NO_1 </b> \n 
+<b>Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ <a href="t.me/VAMPIRE_KING_NO_1"> Tʜɪs Pᴇʀsᴏɴ (https://t.me/Lucifer8880)</a> </b> \n 
 """
 
     RENAME_403_ERR = "What Are You Doing? Bruuh!!"
     BANNED_USER_TEXT = "Sorry!! But Mai Owner As Banned You From Using This Service. That Means You Can't Run Meh Now! \n \n Contact : @VAMPIRE_KING_NO_1 For more Details.. " 
     ABS_TEXT = "What Are You Trying To Do,Mate?"
     UPGRADE_TEXT = "CONTACT @VAMPIRE_KING_NO_1 "
-    DOWNLOAD_START = "<b>Downloading To My server !! pls Wait</b>"
+    DOWNLOAD_START = "<b>Downloading To My server !! Wait</b>"
     UPLOAD_START = "<b>Downloading Completed Now I'm Uploading Into TG</b>"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry Do You Think! I'll Upload It?"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Thank you for Using Me Support Our Channel @storytimeoG</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>I Think You Are Thankful To Me By My Help So Support Our Channel @storytimeoG</b>"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
     NOT_AUTH_USER_TEXT = "CONTACT @VAMPIRE_KING_NO_1 "
     NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @VAMPIRE_KING_NO_1 "
@@ -29,7 +29,7 @@ Go to /help and choose appropriate modes</b> \n
     NO_THUMB_FOUND = "No Thumbnail found"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
-    ABOUT_ME = """<b>Meh : \n An Telegram File Renamer Bot Which Can Rename A Telegram Files. \n You Can Set Permanent Thumbnail For The File So You Don't Have To Send Custom Thumbs All The Time.\n \n i can also convert files into videos \n Feedback @VAMPIRE_KING_NO_1 If You Found Meh Useful</b>"""
+    ABOUT_ME = """<b>Meh : \n Iam 𝘼𝙨𝙝 𝙆𝙚𝙩𝙘𝙝𝙪𝙢 An Telegram File Renamer Bot Which Can Rename A Telegram Files. \n You Can Set Permanent Thumbnail For The File So You Don't Have To Send Custom Thumbs All The Time.\n \n i can also convert files into videos \n Feedback @VAMPIRE_KING_NO_1 If You Found Meh Useful</b>"""
     HELP_USER = """Checkout The Available Commands Here \n\n Do Follow @storytimeoG If You Find This Bot Usefull❤️"""
     RENAME_HELP = """Here are The Available Commands In Reanme \n\n\n▪️ <code>/rename</code> : Reply To An File/video With <code>/rename Filename.extension</code> For Renaming"""
     C2V_HELP = """Here Are The Available Commands In File To Video \n\n\n ▪️<code>/c2v</code> : Reply To An File With /c2v To Convert It Into Video"""
